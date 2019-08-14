90e3f5a7b05dfa3586be6b05f6dab6e207e7e4ff
elswork <elswork@gmail.com>
2019-08-14 13:00:55 +0200

    Update orgs.js
    Add Docker-Caddy

--- js/orgs.js
+++ js/orgs.js
@@ -3,6 +3,10 @@ var orgs = [
        "name": "ml-hispano/mlh_openbci",
        "type": "repo"
   },
+  {
+       "name": "deftwork/docker-caddy",
+       "type": "repo"
+  },
   {   "name": "DeftWork",
       "type": "org"
   }
